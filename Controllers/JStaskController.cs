c86c736d2c1e9a23713ee3fe0f975124e2130484
satya chelluri <118928185+SatyaChelluri7@users.noreply.github.com>
2023-06-02 15:10:50 +0530

    Add files via upload

---
+++ Controllers/JStaskController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Javascripttask.Controllers
+{
+    public class JStaskController : Controller
+    {
+        // GET: JStask
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+
+    }
+}
